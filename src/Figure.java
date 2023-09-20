6f0c7da9903e52d51dd6e527c012dd3a568e545b
klm31415 <klm31415@gmail.com>
2023-09-20 17:47:03 +0300

    to do homework

---
+++ src/Figure.java
@@ -0,0 +1,5 @@
+public interface Figure {
+    double getArea();
+    double Pi = 3.1415;
+
+}
---
+++ src/allArea.java
@@ -0,0 +1,5 @@
+public interface allArea {
+    void getAllArea(double area1, double area2, double area3);
+
+    void getAllArea();
+}
